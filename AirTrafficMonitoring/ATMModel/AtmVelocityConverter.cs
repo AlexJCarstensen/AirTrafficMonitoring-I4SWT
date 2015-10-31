afe459843176f15007449aefeb2124f453feb067
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-31 15:13:47 +0100

    Added IATMVelocityConverter and implemented it

---
+++ AirTrafficMonitoring/ATMModel/AtmVelocityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ATMModel
+{
+    public class ATMVelocityConverter : IATMVelocityConverter
+    {
+        public ATMVelocityConverter(){}
+        public double Convert(IATMCoordinate oldCoordinate, IATMCoordinate newCoordinate, IATMTransponderData oldTimestamp, IATMTransponderData newTimestamp)
+        {
+            var velocity =
+                Math.Sqrt(Math.Pow((newCoordinate.X - oldCoordinate.X), 2) +
+                          Math.Pow((newCoordinate.Y - oldCoordinate.Y), 2));
+            velocity = velocity/
+                       ((double.TryParse(newTimestamp.Timestamp) - double.TryParse(oldTimestamp.Timestamp))/1000);
+            return velocity;
+        }
+    }
+}
---
+++ AirTrafficMonitoring/ATMModel/IATMVelocityConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace ATMModel
+{
+    public interface IATMVelocityConverter
+    {
+        double Convert(IATMCoordinate oldCoordinate, IATMCoordinate newCoordinate, IATMTransponderData oldTimestamp, IATMTransponderData newTimestamp)
+    }
+}
