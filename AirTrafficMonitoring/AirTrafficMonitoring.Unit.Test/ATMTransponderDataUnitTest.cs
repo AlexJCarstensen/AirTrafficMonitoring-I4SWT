2db4e96bfeff7986c72792b4e02b347df5d42a3f
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-31 16:43:37 +0100

    Added ATMTransponderUnitTest and a test

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/ATMTransponderDataUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using ATMModel;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Unit.Test
+{
+    [TestFixture]
+    public class ATMTransponderDataUnitTest
+    {
+        private ATMTransponderData _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ATMTransponderData("F22", 2, 3, 4, "20150512145712542", 25, 54);
+        }
+
+        [Test]
+        public void Tag_returnTrue()
+        {
+            Assert.That(_uut.Tag, Is.EqualTo("F22"));
+        }
+    }
+}
