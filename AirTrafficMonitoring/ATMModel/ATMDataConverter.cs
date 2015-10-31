77295a381b9e0ec8504006b03fba0ba2a5cdeecd
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-31 17:39:26 +0100

    added IATMDataConverter and implemented it

---
+++ AirTrafficMonitoring/ATMModel/ATMDataConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ATMModel
+{
+    public class ATMDataConverter : IATMDataConverter
+    {
+        private static readonly List<IATMTransponderData> LocaDataItems = new List<IATMTransponderData>();
+        public ATMDataConverter(IATMTransponderData data, IATMAngleConverter angle, IATMVelocityConverter velocity)
+        {
+        }
+        public List<IATMTransponderData> Convert(List<string> list) 
+        {
+            if(LocaDataItems.Count > 0) 
+                LocaDataItems.Clear();
+            foreach (var data in list.Select(item => item.Split(';')))
+            {
+                LocaDataItems.Add(new ATMTransponderData(
+                    data[0],
+                    int.Parse(data[1]),
+                    int.Parse(data[2]),
+                    int.Parse(data[3]),
+                    data[4], 0, 0
+                    ));
+            }
+            return LocaDataItems;
+            
+        }
+    }
+}
---
+++ AirTrafficMonitoring/ATMModel/IATMDataConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ATMModel
+{
+    public interface IATMDataConverter
+    {
+        List<IATMTransponderData> Convert(List<string> list );
+    }
+}
