a3b2fe74ff41d311c92cd058397147aa26076880
Alex Carstensen <matthacker.alex@gmail.com>
2015-10-31 14:06:22 +0100

    added IATMCoordinate and Implemented it

---
+++ AirTrafficMonitoring/ATMModel/AtmCoordinate.cs
@@ -0,0 +1,18 @@
+using System.Xml;
+
+namespace ATMModel
+{
+    class ATMCoordinate : IATMCoordinate
+    {
+        public ATMCoordinate(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public bool Validate => X >= 10000 && X <= 90000 && Y >= 10000 && Y <= 90000 && Z >= 500 && Z <= 20000;
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
--- AirTrafficMonitoring/ATMModel/IATMCoordinate.cs
+++ AirTrafficMonitoring/ATMModel/IATMCoordinate.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ATMModel
 {
-    public class IATMCoordinate
+    public interface IATMCoordinate
     {
+        bool Validate { get; }
+        int X { get; set; }
+        int Y { get; set; }
+        int Z { get; set; }
+
     }
 }
