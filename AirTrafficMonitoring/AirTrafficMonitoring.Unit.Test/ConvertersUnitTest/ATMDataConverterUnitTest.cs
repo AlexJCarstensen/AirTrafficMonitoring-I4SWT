170061d3bbddd54bf02a38265fe2fd4902868433
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-03 14:46:25 +0100

    Added Unit tests with NSubstitute ADMDataConverterUnitTest DONE!

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/ConvertersUnitTest/ATMDataConverterUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/ConvertersUnitTest/ATMDataConverterUnitTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ATMModel.Converters;
 using ATMModel.Data;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace AirTrafficMonitoring.Unit.Test.ConvertersUnitTest
@@ -12,28 +13,86 @@ namespace AirTrafficMonitoring.Unit.Test.ConvertersUnitTest
         private IATMDataConverter _uut;
         private IATMAngleConverter _atmAngleConverter;
         private IATMVelocityConverter _atmVelocityConverter;
+
         private List<string> _list;
         private List<IATMTransponderData> _atmTransponderDataList;
 
         [SetUp]
         public void Setup()
         {
+            _uut = new ATMDataConverter(_atmAngleConverter = Substitute.For<IATMAngleConverter>(),
+                _atmVelocityConverter = Substitute.For<IATMVelocityConverter>());
 
-            _uut = new ATMDataConverter(_atmAngleConverter = new ATMAngleConverter(), _atmVelocityConverter = new ATMVelocityConverter());
+            _atmVelocityConverter.Convert(Arg.Any<IATMCoordinate>(), Arg.Any<IATMCoordinate>(), Arg.Any<string>(),
+                Arg.Any<string>()).Returns(15);
+            _atmAngleConverter.Convert(Arg.Any<IATMCoordinate>(), Arg.Any<IATMCoordinate>()).Returns(20);
+           
             _list = new List<string>();
             _atmTransponderDataList = new List<IATMTransponderData>();
             _list.Add("F12;87083;23432;5000;20151012134322345");
             _list.Add("AB34;88083;24432;4321;20151012134323345");
             _list.Add("ABKH2;89083;25432;3423;20151012134324345");
-            _atmTransponderDataList = _uut.Convert(_list);
         }
+
         [Test]
         public void Convert_Add3TransponderData_return3()
         {
+            _atmTransponderDataList = _uut.Convert(_list);
             Assert.That(_atmTransponderDataList.Count, Is.EqualTo(3));
         }
 
         [Test]
+        public void Convert_Tag_ReturnF12()
+        {
+            _atmTransponderDataList = _uut.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(0).Tag, Is.EqualTo("F12"));
+        }
+
+        [Test]
+        public void Convert_CoordX_Return87083()
+        {
+            _atmTransponderDataList = _uut.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(0).Coordinate.X, Is.EqualTo(87083));
+        }
+
+        [Test]
+        public void Convert_CoordY_Return24432()
+        {
+            _atmTransponderDataList = _uut.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(1).Coordinate.Y, Is.EqualTo(24432));
+        }
+
+       [Test]
+        public void Convert_CoordZ_Return3423()
+        {
+            _atmTransponderDataList = _uut.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(2).Coordinate.Z, Is.EqualTo(3423));
+        }
+
+        [Test]
+        public void Convert_CoordTimeStamp_Return20151012134324345()
+        {
+            _atmTransponderDataList = _uut.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(2).Timestamp, Is.EqualTo("20151012134324345"));
+        }
+
+        [Test]
+        public void Convert_Velocity_ConvertReturn15()
+        {
+            var velocityCheck = _uut.Convert(_list);
+            Assert.That(velocityCheck.ElementAt(0).HorizontalVelocity, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void Convert_Angle_ConvertReturn20()
+        {
+            var angleCheck = _uut.Convert(_list);
+            Assert.That(angleCheck.ElementAt(0).CompassCourse, Is.EqualTo(20));
+        }
+    }
+}
+//INTEGRATIONSTEST
+       /* [Test]
         public void ATMDataConverter_Convert_CheckVelocity()
         {
             var locList = new List<string> {"AB34;89083;25432;4321;20151012134326345"};
@@ -47,6 +106,4 @@ namespace AirTrafficMonitoring.Unit.Test.ConvertersUnitTest
             var locList = new List<string> {"AB34;89083;25432;4321;20151012134326345"};
             _atmTransponderDataList = _uut.Convert(locList);
             Assert.That(_atmTransponderDataList.ElementAt(0).CompassCourse, Is.EqualTo(315));
-        }
-    }
-}
+        }*/
