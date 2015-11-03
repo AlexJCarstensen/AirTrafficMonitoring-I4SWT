6e396c4ec5edbe1d7c4b4fe985aff00acc895318
Jeba Ranganathan <jeba477@hotmail.com>
2015-11-03 22:15:44 +0100

    ATMEventHandlerUnitTest added

--- AirTrafficMonitoring/ATMModel/Events/ATMEventHandler.cs
+++ AirTrafficMonitoring/ATMModel/Events/ATMEventHandler.cs
@@ -7,9 +7,16 @@ namespace ATMModel.Events
 {
     public class ATMEventHandler : IATMEventHandler
     {
-        private readonly List<ATMWarning> _atmWarnings = new List<ATMWarning> {new Separation()};
-        private readonly List<ATMNotification> _atmNotifications = new List<ATMNotification> {new TrackEnteredAirspace(), new TrackLeftAirspace()};
+        private readonly List<ATMWarning> _atmWarnings;
+        private readonly List<ATMNotification> _atmNotifications;
         private List<IATMTransponderData> _atmTransponderDatas = new List<IATMTransponderData>();
+
+        public ATMEventHandler(List<ATMWarning> atmWarnings = null, List<ATMNotification> atmNotifications = null)
+        {
+            _atmWarnings = atmWarnings ?? new List<ATMWarning> {new Separation()};
+            _atmNotifications = atmNotifications ?? new List<ATMNotification> { new TrackEnteredAirspace(), new TrackLeftAirspace() };
+        }
+
         public void Handle(List<IATMTransponderData> newAtmTransponderDatas)
         {
             foreach (var warning in _atmWarnings)
---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/EventsUnitTest/ATMEventHandlerUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using ATMModel.Data;
+using ATMModel.Events;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Unit.Test.EventsUnitTest
+{
+    [TestFixture]
+    public class ATMEventHandlerUnitTest
+    {
+        private ATMEventHandler _uut;
+        private ATMWarning _atmWarning;
+        private ATMNotification _atmNotification;
+        private List<IATMTransponderData> _oldAtmTransponderDatas;
+
+        [SetUp]
+        public void Setup()
+        {
+            _atmWarning = Substitute.For<ATMWarning>();
+            _atmNotification = Substitute.For<ATMNotification>();
+            _uut = new ATMEventHandler(new List<ATMWarning> {_atmWarning}, new List<ATMNotification> {_atmNotification});
+            _oldAtmTransponderDatas = new List<IATMTransponderData> {Substitute.For<IATMTransponderData>()};
+            _uut.Handle(_oldAtmTransponderDatas);
+        }
+
+        [Test]
+        public void Handle_CallsATMWarningDetectWarning_withNewData()
+        {
+            var locList = new List<IATMTransponderData>
+            {
+                Substitute.For<IATMTransponderData>(),
+                Substitute.For<IATMTransponderData>(),
+                Substitute.For<IATMTransponderData>()
+            };
+            _uut.Handle(locList);
+            _atmWarning.Received(1).DetectWarning(locList);
+        }
+
+        [Test]
+        public void Handle_CallsATMNotificationDetectNotification_WithOldAndNewList()
+        {
+            var locList = new List<IATMTransponderData>
+            {
+                Substitute.For<IATMTransponderData>(),
+                Substitute.For<IATMTransponderData>(),
+                Substitute.For<IATMTransponderData>()
+            };
+            _uut.Handle(locList);
+            _atmNotification.Received(1).DetectNotification(_oldAtmTransponderDatas, locList);
+        }
+    }
+}
