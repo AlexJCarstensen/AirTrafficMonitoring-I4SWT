34649ede709b4817ed9d061c6c8202944e0f5acd
Jeba Ranganathan <jeba477@hotmail.com>
2015-11-03 20:27:23 +0100

    NotificationEventArgs Fail fixed

--- AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/EventsUnitTest/NotificationEventArgsUnitTest.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Unit.Test/EventsUnitTest/NotificationEventArgsUnitTest.cs
@@ -10,26 +10,28 @@ namespace AirTrafficMonitoring.Unit.Test.EventsUnitTest
     public class NotificationEventArgsUnitTest
     {
         private NotificationEventArgs _notificationEventArgs;
-        private AutoResetEvent EventRaised;
         
         [SetUp]
         public void Setup()
         {
             _notificationEventArgs = new NotificationEventArgs("F12", "testEvent", 100);
-            EventRaised = new AutoResetEvent(false);
-            _notificationEventArgs.StopMeEvent += (sender, eventArgs) => { EventRaised.Set(); };
+            
         }
 
         [Test]
         public void StopMeEvent_NotCalled_Before100milliSecond()
         {
-            Assert.IsFalse(EventRaised.WaitOne(99));
+            AutoResetEvent eventRaised = new AutoResetEvent(false);
+            _notificationEventArgs.StopMeEvent += (sender, eventArgs) => { eventRaised.Set(); };
+            Assert.IsFalse(eventRaised.WaitOne(TimeSpan.FromMilliseconds(99)));
         }
 
         [Test]
         public void StopMeEvent_IsCalled_After100milliSecond()
         {
-            Assert.IsTrue(EventRaised.WaitOne(150));
+            AutoResetEvent eventRaised = new AutoResetEvent(false);
+            _notificationEventArgs.StopMeEvent += (sender, eventArgs) => { eventRaised.Set(); };
+            Assert.IsTrue(eventRaised.WaitOne(TimeSpan.FromMilliseconds(120)));
         }
     }
 }
