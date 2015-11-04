03146fefb5744f87a1f42be749206b880254d9be
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-04 09:26:50 +0100

    added one more test case and added ATMDataDecoderIntegrationTest.cs

---
+++ AirTrafficMonitoring/AirTrafficMonitoring.Integration.Test/ATMDataDecoderIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using ATMModel;
+using ATMModel.Converters;
+using ATMModel.Events;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring.Integration.Test
+{
+    [TestFixture]
+    public class ATMDataDecoderIntegrationTest
+    {
+        private IATMDataDecoder _atmDataDecoder;
+        private IATMDataConverter _atmDataConverter;
+        private IATMEventHandler _atmEventHandler;
+        private ITransponderReceiver _transponderReceiver;
+        [SetUp]
+        public void Setup()
+        {
+            //_atmDataDecoder = new ATMDataDecoder(_transponderReceiver =, );
+        }
+    }
+}
