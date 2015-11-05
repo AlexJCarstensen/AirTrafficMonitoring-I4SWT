8ce259e806d4e2c097efc743fc47a9c0fbaf484e
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-05 11:47:39 +0100

    IntegrationTestsone done

--- AirTrafficMonitoring/AirTrafficMonitoring.Integration.Test/ConvertersIntegrationTest/ATMIntegrationTestTwo.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Integration.Test/ConvertersIntegrationTest/ATMIntegrationTestTwo.cs
@@ -1,7 +1,42 @@
-﻿namespace AirTrafficMonitoring.Integration.Test.ConvertersIntegrationTest
+﻿using System.Collections.Generic;
+using System.Linq;
+using ATMModel.Converters;
+using ATMModel.Data;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Integration.Test.ConvertersIntegrationTest
 {
+    [TestFixture]
     public class ATMIntegrationTestTwo
     {
-         
+        private IATMDataConverter _atmDataConverter;
+        private IATMAngleConverter _atmAngleConverter;
+        private IATMTransponderData _atmTransponderData;
+        private List<string> _list;
+        private List<IATMTransponderData> _atmTransponderDataList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _atmDataConverter = new ATMDataConverter(_atmAngleConverter = new ATMAngleConverter(), Substitute.For<IATMVelocityConverter>());
+            _atmTransponderData = new ATMTransponderData("F12", new ATMCoordinate(20453, 46569, 15203), "201505111104253");
+
+            _list = new List<string>
+            {
+                "F12;87083;23432;5000;20151012134322345",
+                "AB34;88083;24432;4321;20151012134323345",
+                "ABKH2;89083;25432;3423;20151012134324345"
+            };
+            _atmTransponderDataList = new List<IATMTransponderData>();
+            
+        }
+        /*
+        [Test]
+        public void ConvertAngle_CheckNorth_return0()
+        {
+            _atmTransponderDataList = _atmDataConverter.Convert(_list);
+            Assert.That(_atmTransponderDataList.ElementAt(0).CompassCourse, Is.EqualTo(20));
+        }*/
     }
 }
--- AirTrafficMonitoring/AirTrafficMonitoring.Integration.Test/DataIntegrationTest/ATMIntegrationTestOne.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring.Integration.Test/DataIntegrationTest/ATMIntegrationTestOne.cs
@@ -1,7 +1,97 @@
-﻿namespace AirTrafficMonitoring.Integration.Test.DataIntegrationTest
+﻿using ATMModel.Data;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Integration.Test.DataIntegrationTest
 {
+    [TestFixture]
     public class ATMIntegrationTestOne
     {
-         
+        private ATMTransponderData _atmTransponderData;
+        private IATMCoordinate _atmCoordinate;
+        [SetUp]
+        public void Setup()
+        {
+            _atmTransponderData = new ATMTransponderData("F12", new ATMCoordinate(64251, 89857, 19857), "201505111104253" );
+        }
+
+        [Test]
+        public void GetCoordinateX_From_ATMCoordinate_return64251()
+        {
+            Assert.That(_atmTransponderData.Coordinate.X, Is.EqualTo(64251));
+        }
+
+        [Test]
+        public void GetCoordinateY_From_ATMCoordinate_return89857()
+        {
+            Assert.That(_atmTransponderData.Coordinate.Y, Is.EqualTo(89857));
+        }
+
+        [Test]
+        public void GetCoordinateZ_From_ATMCoordinate_return19857()
+        {
+            Assert.That(_atmTransponderData.Coordinate.Z, Is.EqualTo(19857));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_ReturnTrue()
+        {
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_AllInvalidLow_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData {Coordinate = new ATMCoordinate(5000, 5000, 4999)};
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_AllInvalidHigh_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(92312, 95643, 25643) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_XInvalidLow_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(5000, 45325, 18645) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_XInvalidHigh_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(95684, 45325, 18645) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_YInvalidLow_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(45325, 5000, 18645) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_YInvalidHigh_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(45325, 120356, 18645) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_ZInvalidLow_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(45325, 45325, 123) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ATMCoordinate_Validate_ZInvalidHigh_ReturnFalse()
+        {
+            _atmTransponderData = new ATMTransponderData { Coordinate = new ATMCoordinate(45325, 45325, 22000) };
+            Assert.That(_atmTransponderData.Coordinate.Validate, Is.EqualTo(false));
+        }
     }
 }
